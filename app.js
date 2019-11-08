47234f69832414169de4797010c9fde783f21bd3
Andrey Kornyushko <kap86@ukr.net>
2019-11-08 19:51:42 +0200

    fixed cors

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const { Router } = require('express');
-const corsMiddlware = require('cors');
+const cors = require('cors');
 const expressJwt = require('express-jwt');
 const userDb = require('./utils/db');
 const token = require('./utils/token');
@@ -10,15 +10,7 @@ const token = require('./utils/token');
 
 const app = express();
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-
-app.use(corsMiddlware({
-  origin: 'https://total-name-258415.appspot.com'
-}));
+app.use(cors());
 // app.use(corsMiddlware());
 
 app.use(express.json());
