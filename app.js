a55fe13d9a5277cfa747c0cfb538ef032d75eff9
Andrey Kornyushko <kap86@ukr.net>
2019-11-08 21:12:40 +0200

    fixed cors

--- app.js
+++ app.js
@@ -12,32 +12,40 @@ const app = express();
 
 app.use(cors());
 
-app.use(function(req, res, next) {
-  var oneof = false;
-  if(req.headers.origin) {
-      res.header('Access-Control-Allow-Origin', req.headers.origin);
-      oneof = true;
-  }
-  if(req.headers['access-control-request-method']) {
-      res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
-      oneof = true;
-  }
-  if(req.headers['access-control-request-headers']) {
-      res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
-      oneof = true;
-  }
-  if(oneof) {
-      res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
-  }
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
 
-  // intercept OPTIONS method
-  if (oneof && req.method == 'OPTIONS') {
-      res.send(200);
-  }
-  else {
-      next();
-  }
-});
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
+
+// app.use(function(req, res, next) {
+//   var oneof = false;
+//   if(req.headers.origin) {
+//       res.header('Access-Control-Allow-Origin', req.headers.origin);
+//       oneof = true;
+//   }
+//   if(req.headers['access-control-request-method']) {
+//       res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
+//       oneof = true;
+//   }
+//   if(req.headers['access-control-request-headers']) {
+//       res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
+//       oneof = true;
+//   }
+//   if(oneof) {
+//       res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
+//   }
+
+//   // intercept OPTIONS method
+//   if (oneof && req.method == 'OPTIONS') {
+//       res.send(200);
+//   }
+//   else {
+//       next();
+//   }
+// });
 
 
 // app.options('*', cors());
