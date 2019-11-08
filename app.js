77f28fe3304a14d1bd5fe64ae577f3c46caa5acf
Andrey Kornyushko <kap86@ukr.net>
2019-11-08 18:18:18 +0200

    fixed cors

--- app.js
+++ app.js
@@ -10,11 +10,15 @@ const token = require('./utils/token');
 
 const app = express();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+
+app.use(corsMiddlware({
+  origin: 'https://total-name-258415.appspot.com'
+}));
 // app.use(corsMiddlware());
 
 app.use(express.json());
